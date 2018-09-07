99f1569057c9bea015a9950a47d3a5aac1856257
Oliver Hampton <oliverhampton@gmail.com>
2018-09-07 11:01:34 -0400

    Update getMappingSheet.sh

--- bin/getMappingSheet.sh
+++ bin/getMappingSheet.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export MAPBIN=/home/socci/Code/Inventory/mkMapFiles/HiSeq
+#export MAPBIN=/home/socci/Code/Inventory/mkMapFiles/HiSeq
 SAMPLEDIRS=$1
-cat $SAMPLEDIRS | xargs -n 1 $MAPBIN/isPEorSERun.sh >sampleManifest 
-$MAPBIN/cvtManifest2Mapfile.py sampleManifest 
+#cat $SAMPLEDIRS | xargs -n 1 $MAPBIN/isPEorSERun.sh >sampleManifest 
+#$MAPBIN/cvtManifest2Mapfile.py sampleManifest
+cat $SAMPLEDIRS | xargs -n 1 isPEorSERun.sh >sampleManifest 
+cvtManifest2Mapfile.py sampleManifest
 #rm sampleManifest
